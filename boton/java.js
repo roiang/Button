bf2038803decf4d40b4d25f659ceb4589c58ee3c
Omar278 <65314510+Omar278@users.noreply.github.com>
2020-09-14 13:44:08 -0500

    Add files via upload

---
+++ boton/java.js
@@ -0,0 +1,11 @@
+var a= document.querySelector(getElementById= "bgcolor")
+document.getElementById("bgcolor").innerHTML = "Hola Marte!!!!";
+
+
+  addEventListener('click', function() {
+    if(bgcolor==white)
+    {
+        bgcolor=red
+    }else{
+       bgcolor=white
+    };
